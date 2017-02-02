7ac7e846b8e24599399ef130c7fe8e6532f062b8
Jesse Gumm <gumm@sigma-star.com>
2017-02-01 18:06:06 -0600

    Update example file

--- example/test.erl
+++ example/test.erl
@@ -3,13 +3,14 @@
 
 -export([do/0]).
 
--record(testrec, {a=5, b = <<1,2>>, c="mystr", x={tuple}, y=[7,b]}).
+-record(testrec, {z, zz :: any(), a=5 :: integer() | list(), b = <<1,2>>, c="mystr", x={tuple}, y=[7,b]}).
 
 -extend({testrec, testrec1, [
     {d, "what"},
     e,
     {f, 0.1},
-    {h, undefined}
+    {h, undefined},
+    {zz, "some string", "string()"}
 ]}).
 
 do() ->
